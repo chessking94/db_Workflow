d750def67db8fe91d4591405a1dc2864397fa29b
Ethan Hunt <chessking94@gmail.com>
2024-07-20 12:51:35 -0500

    Initial base workflow table

---
+++ db_Workflow/dbo/Tables/Workflows.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Workflows]
+(
+	[workflowID] INT IDENTITY(1,1) NOT NULL,
+	[workflowName] VARCHAR(20) NOT NULL,
+	[workflowDescription] VARCHAR(100) NOT NULL,
+	CONSTRAINT [PK_Workflows] PRIMARY KEY CLUSTERED ([workflowID] ASC),
+	CONSTRAINT [UC_Workflows_Name] UNIQUE NONCLUSTERED ([workflowName] ASC)
+);
