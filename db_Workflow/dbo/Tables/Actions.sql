a8e10d9f6f19232ac422d776a4650d79a2f8a54f
Ethan Hunt <chessking94@gmail.com>
2024-07-20 13:00:53 -0500

    Initial draft of Actions table (will need more fields)

---
+++ db_Workflow/dbo/Tables/Actions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Actions]
+(
+	[actionID] INT IDENTITY(1,1) NOT NULL,
+	[actionName] VARCHAR(20) NOT NULL,
+	[actionDescription] VARCHAR(100) NOT NULL,
+	CONSTRAINT [PK_Actions] PRIMARY KEY CLUSTERED ([actionID] ASC),
+	CONSTRAINT [UC_Actions_Name] UNIQUE NONCLUSTERED ([actionName] ASC)
+)
