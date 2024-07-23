b97f9aa98398d82293120a65a925cd1e9edf421c
Ethan Hunt <chessking94@gmail.com>
2024-07-22 20:15:33 -0500

    Delete workflows table, won't be needed

--- db_Workflow/dbo/Tables/Workflows.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Workflows]
-(
-	[workflowID] INT IDENTITY(1,1) NOT NULL,
-	[workflowName] VARCHAR(20) NOT NULL,
-	[workflowDescription] VARCHAR(100) NOT NULL,
-	[workflowActive] BIT CONSTRAINT [DF_Workflows_Active] DEFAULT ((0)) NOT NULL,
-	CONSTRAINT [PK_Workflows] PRIMARY KEY CLUSTERED ([workflowID] ASC),
-	CONSTRAINT [UC_Workflows_Name] UNIQUE NONCLUSTERED ([workflowName] ASC)
-);
