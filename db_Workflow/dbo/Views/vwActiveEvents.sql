f100732cdb68a27f6090bb52bcaa72d43a5eddd2
Ethan Hunt <chessking94@gmail.com>
2024-08-04 15:54:59 -0500

    Add view for active events

---
+++ db_Workflow/dbo/Views/vwActiveEvents.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW [dbo].[vwActiveEvents]
+
+AS
+
+SELECT
+e.eventID,
+app.applicationName,
+w.workflowName,
+wa.stepNumber,
+a.actionName,
+es.eventStatus,
+e.eventStatusDate,
+e.eventStartDate
+
+FROM dbo.Events e
+JOIN dbo.EventStatuses es ON
+	e.eventStatusID = es.eventStatusID
+JOIN dbo.Actions a ON
+	e.actionID = a.actionID
+LEFT JOIN dbo.Applications app ON
+	a.applicationID = app.applicationID
+LEFT JOIN dbo.WorkflowActions wa ON
+	e.workflowID = wa.workflowID
+	AND e.stepNumber = wa.stepNumber
+LEFT JOIN dbo.Workflows w ON
+	wa.workflowID = w.workflowID
+
+WHERE es.isTerminal = 0
