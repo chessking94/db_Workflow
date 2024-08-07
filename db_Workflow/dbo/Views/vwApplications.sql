ee9ae0d7db06d234a6aa5ab7bb5a87777184ee0b
Ethan Hunt <chessking94@gmail.com>
2024-08-06 22:17:01 -0500

    Add view for applications

---
+++ db_Workflow/dbo/Views/vwApplications.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [dbo].[vwApplications]
+
+AS
+
+SELECT
+applicationID,
+applicationName,
+applicationDescription,
+applicationFilename,
+applicationDefaultParameter,
+applicationActive
+
+FROM dbo.Applications
