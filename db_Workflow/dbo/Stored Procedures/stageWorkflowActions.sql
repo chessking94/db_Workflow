e75604cc7ccb10ce269709ab30311c48b1f099d4
Ethan Hunt <chessking94@gmail.com>
2024-08-14 22:03:45 -0500

    First proc for WorkflowActions

---
+++ db_Workflow/dbo/Stored Procedures/stageWorkflowActions.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[stageWorkflowActions] (
+	@workflowName VARCHAR(20)
+)
+
+AS
+
+BEGIN
+	DELETE stg
+	FROM dbo.stage_WorkflowActions stg
+	JOIN dbo.Workflows wf ON
+		stg.workflowID = wf.workflowID
+	WHERE wf.workflowName = @workflowName
+
+	INSERT INTO dbo.stage_WorkflowActions (workflowID, stepNumber, actionID, eventParameters, continueAfterError)
+	SELECT
+	wa.workflowID,
+	wa.stepNumber,
+	wa.actionID,
+	wa.eventParameters,
+	wa.continueAfterError
+
+	FROM dbo.WorkflowActions wa
+	JOIN dbo.Workflows wf ON
+		wa.workflowID = wf.workflowID
+	WHERE wf.workflowName = @workflowName
+END
