6bcdf71e1f07b10e283ce65997f06ff545b7a640
Ethan Hunt <chessking94@gmail.com>
2024-08-16 22:44:44 -0500

    Add surrogate key

--- db_Workflow/dbo/Tables/stage_WorkflowActions.sql
+++ db_Workflow/dbo/Tables/stage_WorkflowActions.sql
@@ -1,11 +1,13 @@
 ﻿CREATE TABLE [dbo].[stage_WorkflowActions]
 (
+	[stagingKey] INT IDENTITY(1,1) NOT NULL,
 	[workflowID] SMALLINT NOT NULL,
 	[stepNumber] TINYINT NOT NULL,
 	[actionID] INT NOT NULL,
 	[eventParameters] VARCHAR(250) NULL,
 	[continueAfterError] BIT NOT NULL,
 	--TODO: [runConcurrent] BIT NOT NULL,
+	CONSTRAINT [PK_sWorkflowActions] PRIMARY KEY ([stagingKey]),
 	CONSTRAINT [FK_sWorkflowActions_Workflows] FOREIGN KEY ([workflowID]) REFERENCES dbo.Workflows([workflowID]),
 	CONSTRAINT [FK_sWorkflowActions_Actions] FOREIGN KEY ([actionID]) REFERENCES dbo.Actions([actionID])
 )
