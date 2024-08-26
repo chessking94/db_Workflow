cddfe52138b124e2cfd9ae621cd41e902c81cfc2
Ethan Hunt <chessking94@gmail.com>
2024-08-25 22:31:42 -0500

    Improve flexibility of createEvent

--- db_Workflow/dbo/Stored Procedures/createEvent.sql
+++ db_Workflow/dbo/Stored Procedures/createEvent.sql
@@ -1,6 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[createEvent] (
-	@workflowID SMALLINT = NULL,
-	@stepNumber TINYINT = NULL,
+	@workflowName VARCHAR(50) = NULL,
 	@actionName VARCHAR(50) = NULL,
 	@eventParameters VARCHAR(250) = NULL,
 	@eventStartDate DATETIME = NULL
@@ -8,74 +7,72 @@
 
 AS
 
-DECLARE @errmsg NVARCHAR(128)
+DECLARE @workflowID SMALLINT
 DECLARE @actionID INT
 
-IF (@workflowID IS NOT NULL OR @stepNumber IS NOT NULL)
+SET @workflowName = NULLIF(@workflowName, '')
+SET @actionName = NULLIF(@actionName, '')
+SET @eventParameters = NULLIF(@eventParameters, '')
+
+IF (@workflowName IS NULL AND @actionName IS NULL) RETURN -1  --no workflow or action name passed
+
+IF @workflowName IS NOT NULL
 BEGIN
-	--if workflow info is provided, override the provided actionName with what is associated with the workflow step
-	SET @actionID = (SELECT actionID FROM dbo.WorkflowActions WHERE workflowID = @workflowID AND stepNumber = @stepNumber)
-	IF @actionID IS NOT NULL
+	SELECT @workflowID = workflowID FROM dbo.Workflows WHERE workflowName = @workflowName
+	IF @workflowID IS NOT NULL
 	BEGIN
-		SET @actionName = @actionID
-	END
+		IF (SELECT COUNT(stepNumber) FROM dbo.WorkflowActions WHERE workflowID = @workflowID) = 0 RETURN -2  --no actions set up for this workflow
 
-	ELSE
-
-	BEGIN
-		--invalid workflow/step number combination
-		RETURN -1
-	END
-END
-
-IF @errmsg IS NULL
-BEGIN
-	IF ISNUMERIC(@actionName) = 1
-	BEGIN
-		IF EXISTS (SELECT actionID FROM dbo.Actions WHERE actionID = @actionName)
+		IF @eventStartDate IS NULL
 		BEGIN
-			IF @eventStartDate IS NULL
-			BEGIN
-				INSERT INTO dbo.Events (actionID, eventParameters)
-				SELECT @actionName, @eventParameters
-			END
-
-			ELSE
-
-			BEGIN
-				INSERT INTO dbo.Events (actionID, eventParameters, eventStartDate)
-				SELECT @actionName, @eventParameters, @eventStartDate
-			END
-		END
-	END
-
-	ELSE
-
-	BEGIN
-		SET @actionID = (SELECT actionID FROM dbo.Actions WHERE actionName = @actionName)
-		IF @actionID IS NOT NULL
-		BEGIN
-			IF @eventStartDate IS NULL
-			BEGIN
-				INSERT INTO dbo.Events (actionID, eventParameters)
-				SELECT @actionID, @eventParameters
-			END
-
-			ELSE
-
-			BEGIN
-				INSERT INTO dbo.Events (actionID, eventParameters, eventStartDate)
-				SELECT @actionID, @eventParameters, @eventStartDate
-			END
+			INSERT INTO dbo.Events (workflowID, stepNumber, actionID, eventParameters)
+			SELECT workflowID, stepNumber, actionID, eventParameters FROM dbo.WorkflowActions WHERE workflowID = @workflowID ORDER BY stepNumber
 		END
 
 		ELSE
 
 		BEGIN
-			--actionName parameter not a key or action name
-			RETURN -2
+			INSERT INTO dbo.Events (workflowID, stepNumber, actionID, eventParameters, eventStartDate)
+			SELECT workflowID, stepNumber, actionID, eventParameters, @eventStartDate FROM dbo.WorkflowActions WHERE workflowID = @workflowID ORDER BY stepNumber
 		END
+
+		RETURN @@ROWCOUNT  --return number of actions instead of last ID
+	END
+
+	ELSE
+
+	BEGIN
+		--invalid workflow name
+		RETURN -3
 	END
 END
 
-RETURN @@IDENTITY
+ELSE
+
+BEGIN
+	SELECT @actionID = actionID FROM dbo.Actions WHERE actionName = @actionName
+	IF @actionID IS NOT NULL
+	BEGIN
+		IF @eventStartDate IS NULL
+		BEGIN
+			INSERT INTO dbo.Events (actionID, eventParameters)
+			SELECT @actionID, @eventParameters
+		END
+
+		ELSE
+
+		BEGIN
+			INSERT INTO dbo.Events (actionID, eventParameters, eventStartDate)
+			SELECT @actionID, @eventParameters, @eventStartDate
+		END
+
+		RETURN @@IDENTITY
+	END
+
+	ELSE
+
+	BEGIN
+		--invalid action name
+		RETURN -4
+	END
+END
