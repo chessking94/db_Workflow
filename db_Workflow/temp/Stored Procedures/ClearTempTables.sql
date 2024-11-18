3ddb142b9a2153db1462dc7b3b79cb819d464995
Ethan Hunt <chessking94@gmail.com>
2024-11-17 20:46:16 -0600

    Procedure for clearing tables in the temp schema

---
+++ db_Workflow/temp/Stored Procedures/ClearTempTables.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [temp].[ClearTempTables]
+
+AS
+
+/*
+	The intent of this procedure is to be executed *once per day* as part of a clean-up night job.
+	Due to that, it is written in such a way to not impact any record associated with today's date.
+*/
+
+BEGIN
+	--temp.ErrorReviewCompleted
+	DELETE t
+	FROM temp.ErrorReviewCompleted t
+	JOIN dbo.Events e ON
+		t.eventID = e.eventID
+	WHERE e.eventEndDate < CAST(GETDATE() AS DATE)
+END
